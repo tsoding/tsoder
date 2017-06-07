7e6e9e731933bde20f8e1fa23f6f78d23b9f099b
rexim <reximkut@gmail.com>
2017-06-08 00:44:37 +0700

    Add FSM for bot (#10)

---
+++ src/tsoding_bot_bot.erl
@@ -0,0 +1,24 @@
+-module(tsoding_bot_bot).
+-behaviour(gen_fsm).
+-export([start_link/0,
+         listen/2,
+         init/1,
+         terminate/3]).
+
+start_link() ->
+    gen_fsm:start_link(?MODULE, listen, [{debug, [trace]}]).
+
+init(State) ->
+    {ok, State, {}}.
+
+terminate(Reason, StateName, StateData) ->
+    error_logger:info_report([{reason, Reason}]).
+
+listen({message, Message}, {}) ->
+    error_logger:info_report([{message, Message}]),
+    {next_state, listen, {}};
+listen(Event, {}) ->
+    error_logger:info_report([{unknown_event, Event}]),
+    {next_state, listen, {}}.
+
+
