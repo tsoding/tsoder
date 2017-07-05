233cd3bce9845a3bf86d009297a0f978c6aae1ff
rexim <reximkut@gmail.com>
2017-07-06 01:56:13 +0700

    Unlink TODO(#21)

--- src/tsoder_app.erl
+++ src/tsoder_app.erl
@@ -15,18 +15,6 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    %% TODO(#21): Organize transport-bot pipeline.
-    %%
-    %% - Supersedes #13
-    %%
-    %% On application startup create IRC transport instance (see
-    %% `tsoding_irc_transport` module) and Tsoder Bot instance (see
-    %% `tsoder_bot` module) and organize a pipeline out of them so
-    %% 1. The transport receives IRC messages from SSL socket,
-    %% 2. The transport forwards the messages to the bot,
-    %% 3. The messages that are forwareded to the bot should be transport
-    %% agnostic.
-    %% 4. The bot logs the received messages.
     tsoder_sup:start_link().
 
 %%--------------------------------------------------------------------
