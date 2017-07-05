94bada242c81b155f27f0b5cf131ad7400a0f289
rexim <reximkut@gmail.com>
2017-07-06 01:08:00 +0700

    Make irc_commands module scalable (#21)

--- src/irc_commands.erl
+++ src/irc_commands.erl
@@ -1,13 +1,36 @@
 -module(irc_commands).
 -export([line_as_irc_command/1]).
 
+regexp_matched_command(Line, RegexpString, Extractor) ->
+    {ok, Regexp} = re:compile(RegexpString),
+    case re:run(Line, Regexp, [{capture, all, list}]) of
+        {match, Groups} ->
+            {ok, Extractor(Groups)};
+        nomatch ->
+            {error, {unsupported_command}}
+    end.
+
+line_as_ping_command(Line) ->
+    regexp_matched_command(
+      Line,
+      "PING (.*)",
+      fun([_, Host]) -> {ping, Host} end).
+
+line_as_privmsg_command(Line) ->
+    regexp_matched_command(
+      Line,
+      ":.+ PRIVMSG #.+ :(.*)$",
+      fun([_, Host]) -> {privmsg, Host} end).
+
 line_as_irc_command(Line) ->
-    {ok, PingRegex} = re:compile("PING (.*)"),
-    {ok, PrivmsgRegex} = re:compile(":.+ PRIVMSG #.+ :(.*)$"),
-    case re:run(Line, PingRegex, [{capture, all, list}]) of
-        {match, [_, Host]} -> {ok, {ping, Host}};
-        nomatch -> case re:run(Line, PrivmsgRegex, [{capture, all, list}]) of
-                       {match, [_, Msg]} -> {ok, {privmsg, Msg}};
-                       nomatch -> {error, {unsupported_command}}
-                   end
+    Commands = [fun line_as_ping_command/1,
+                fun line_as_privmsg_command/1],
+    Result = lists:dropwhile(
+               fun (Command) -> not option:defined(Command(Line)) end,
+               Commands),
+    case Result of
+        [] ->
+            {error, {unsupported_command}};
+        [Command|_] ->
+            Command(Line)
     end.
