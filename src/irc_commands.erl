bfa75df4fe18b5ff5cf1bc1c2c8ceeac905ed883
rexim <reximkut@gmail.com>
2017-07-06 00:03:49 +0700

    Basic support for PRIVMSG IRC command (#21)

--- src/irc_commands.erl
+++ src/irc_commands.erl
@@ -3,7 +3,11 @@
 
 line_as_irc_command(Line) ->
     {ok, PingRegex} = re:compile("PING (.*)"),
+    {ok, PrivmsgRegex} = re:compile(":.+ PRIVMSG #.+ :(.*)$"),
     case re:run(Line, PingRegex, [{capture, all, list}]) of
         {match, [_, Host]} -> {ok, {ping, Host}};
-        nomatch -> {error, {unsupported_command}}
+        nomatch -> case re:run(Line, PrivmsgRegex, [{capture, all, list}]) of
+                       {match, [_, Msg]} -> {ok, {privmsg, Msg}};
+                       nomatch -> {error, {unsupported_command}}
+                   end
     end.
