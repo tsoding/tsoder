b47ab53997aac5a88aaa4d36c85eca774dbbd2b8
rexim <reximkut@gmail.com>
2017-07-07 13:44:27 +0700

    Negative case for option_tests:option_test/0

--- test/option_tests.erl
+++ test/option_tests.erl
@@ -4,4 +4,7 @@
 map_test() ->
     ?assertMatch({ok, 5},
                  option:map((fun(X) -> X + 1 end),
-                            {ok, 4})).
+                            {ok, 4})),
+    ?assertMatch({error, "Failed"},
+                 option:map((fun(X) -> X + 1 end),
+                            {error, "Failed"})).
