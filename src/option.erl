0d0213da4bff4d28aca29f00a1c3eaf78de90c6b
rexim <reximkut@gmail.com>
2017-07-07 13:45:37 +0700

    Refactor option:map/2

--- src/option.erl
+++ src/option.erl
@@ -3,11 +3,10 @@
          flat_map/2,
          defined/1]).
 
-map(F, O) ->
-    case O of
-        {ok, Value} -> {ok, F(Value)};
-        None -> None
-    end.
+map(F, {ok, Value}) ->
+    {ok, F(Value)};
+map(F, None) ->
+    None.
 
 flat_map(F, O) ->
     case O of
