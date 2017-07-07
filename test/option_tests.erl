3507aa1513b142ec045b833e7727590044f9915e
rexim <reximkut@gmail.com>
2017-07-07 13:43:01 +0700

    Add option_tests:map_test/0

---
+++ test/option_tests.erl
@@ -0,0 +1,7 @@
+-module(option_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+map_test() ->
+    ?assertMatch({ok, 5},
+                 option:map((fun(X) -> X + 1 end),
+                            {ok, 4})).
