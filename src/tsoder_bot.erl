ff439c5adb6aaf8b4109ab7646790f41660c9e48
rexim <reximkut@gmail.com>
2017-07-13 00:27:30 +0700

    Implement the hi command (#38)

--- src/tsoder_bot.erl
+++ src/tsoder_bot.erl
@@ -14,13 +14,21 @@ init(State) ->
 terminate(Reason, StateName, StateData) ->
     error_logger:info_report([{reason, Reason}]).
 
-listen({message, Message}, Data) ->
+listen({message, Message}, Channel) ->
     error_logger:info_report([{message, Message}]),
-    {next_state, listen, Data};
+    case user_command:of_string(Message) of
+        {ok, {"hi", []}} ->
+            Channel ! {message, "Hello there!"},
+            {next_state, listen, Channel};
+        {ok, {"hi", Name}} ->
+            Channel ! {message, "Hello " ++ Name ++ "!"},
+            {next_state, listen, Channel};
+        _ -> {next_state, listen, Channel}
+    end;
 listen({join, Channel}, Data) ->
     error_logger:info_report([{join, Channel}]),
     Channel ! {message, "Hello from Tsoder again!"},
-    {next_state, listen, {ok, Channel}};
+    {next_state, listen, Channel};
 listen(Event, Data) ->
     error_logger:info_report([{unknown_event, Event}]),
     {next_state, listen, Data}.
