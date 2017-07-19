b13aa358d0da47e1a129014222208de6d325f9e6
rexim <reximkut@gmail.com>
2017-07-19 16:27:22 +0700

    Collect logs to a folder (#41)

--- src/tsoder_app.erl
+++ src/tsoder_app.erl
@@ -14,7 +14,19 @@
 %% API
 %%====================================================================
 
+log_file_name(Suffix) ->
+    string:join(["log_", Suffix, ".txt"], "").
+
 start(_StartType, _StartArgs) ->
+    LogFilePath =
+        filename:join(
+          "./logs",
+          log_file_name(
+            utc_timestamp:as_string(
+              calendar:now_to_universal_time(
+                erlang:timestamp())))),
+    filelib:ensure_dir(LogFilePath),
+    error_logger:logfile({open, LogFilePath}),
     tsoder_sup:start_link().
 
 %%--------------------------------------------------------------------
