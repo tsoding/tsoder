f9238074b637d83e3f53ed8585200fcb757441ee
rexim <reximkut@gmail.com>
2017-08-09 01:28:51 +0700

    Use record as the bot state (#64)
    That makes it easier to extend the state. We don't have to update all
    of the possible pattern matchings anymore. Generally I like to avoid
    pattern matchings at all.

--- src/tsoder_bot.erl
+++ src/tsoder_bot.erl
@@ -6,48 +6,53 @@
          handle_cast/2,
          terminate/2]).
 
+-record(state, {channel = nothing,
+                command_table = #{
+                  "hi"   => { fun hi_command/3, "!hi -- says hi to you" },
+                  "help" => { fun help_command/3, "!help [command] -- prints the list of supported commands" },
+                  "fart" => { fun fart_command/3, "!fart [rating] -- fart" }
+                 },
+                fart_rating = #{}
+               }).
+
 start_link() ->
     gen_server:start_link({local, tsoder_bot}, ?MODULE, [], [{debug, [trace]}]).
 
 init([]) ->
-    {ok,
-     {nothing,
-      #{ "hi"   => { fun hi_command/3, "!hi -- says hi to you" },
-         "help" => { fun help_command/3, "!help [command] -- prints the list of supported commands" },
-         "fart" => { fun fart_command/3, "!fart -- fart" } }}}.
+    {ok, #state{}}.
 
 terminate(Reason, State) ->
     error_logger:info_report([{reason, Reason},
                               {state, State}]).
 
-hi_command({MaybeChannel, _}, User, _) ->
+hi_command(State, User, _) ->
     option:foreach(
       fun (Channel) ->
               Channel ! {message, "Hello @" ++ User ++ "!"}
       end,
-      MaybeChannel).
+      State#state.channel).
 
-fart_command({MaybeChannel, _}, User, _) ->
+fart_command(State, User, _) ->
     option:foreach(
       fun (Channel) ->
               Channel ! {message,
                          "@" ++ User ++ ", don't have intestines to perform the operation, sorry."
                         }
       end,
-      MaybeChannel).
+      State#state.channel).
 
-help_command({MaybeChannel, CommandTable}, User, "") ->
+help_command(State, User, "") ->
    option:foreach(
      fun (Channel) ->
              Channel ! {message,
-                        "@" ++ User ++ ", supported commands: " ++ string:join(maps:keys(CommandTable), ", ")
+                        "@" ++ User ++ ", supported commands: " ++ string:join(maps:keys(State#state.command_table), ", ")
                        }
      end,
-     MaybeChannel);
-help_command({MaybeChannel, CommandTable}, User, Command) ->
+     State#state.channel);
+help_command(State, User, Command) ->
     option:foreach(
       fun (Channel) ->
-              case CommandTable of
+              case State#state.command_table of
                   #{ Command := {_, Description} } ->
                       Channel ! {message,
                                  "@" ++ User ++ ", " ++ Description
@@ -58,21 +63,19 @@ help_command({MaybeChannel, CommandTable}, User, Command) ->
                                 }
               end
       end,
-     MaybeChannel).
+     State#state.channel).
 
 handle_call(_, _, State) ->
     {reply, unsupported, State}.
 
-handle_cast({message, User, Message}, {MaybeChannel, CommandTable}) ->
+handle_cast({message, User, Message}, State) ->
     error_logger:info_report([{message, Message}]),
 
     option:foreach(
       fun ({Command, Arguments}) ->
-              case CommandTable of
+              case State#state.command_table of
                   #{ Command := { Action, _ } } ->
-                      Action({MaybeChannel, CommandTable},
-                             User,
-                             Arguments);
+                      Action(State, User, Arguments);
                   _ ->
                       error_logger:info_report({unsupported_command,
                                                 {Command, Arguments}})
@@ -80,11 +83,11 @@ handle_cast({message, User, Message}, {MaybeChannel, CommandTable}) ->
       end,
       user_command:of_string(Message)),
 
-    {noreply, {MaybeChannel, CommandTable}};
-handle_cast({join, Channel}, {_, CommandTable}) ->
+    {noreply, State};
+handle_cast({join, Channel}, State) ->
     error_logger:info_report([{join, Channel}]),
     Channel ! {message, "Hello from Tsoder again!"},
-    {noreply, {{ok, Channel}, CommandTable}};
+    {noreply, State#state{channel = {ok, Channel}}};
 handle_cast(Event, State) ->
     error_logger:info_report([{unknown_event, Event}]),
     {noreply, State}.
