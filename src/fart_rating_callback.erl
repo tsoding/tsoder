4186bae9e1ec704a7cec41913dda2ddf0e8c386a
rexim <reximkut@gmail.com>
2017-09-02 22:45:59 +0700

    Implement fart_rating_callback (#86)

--- src/fart_rating_callback.erl
+++ src/fart_rating_callback.erl
@@ -11,10 +11,59 @@ empty() ->
     #state {}.
 
 from_file(FilePath) ->
-    #state {}.
+    #state{ file_path = {ok, FilePath},
+            fart_rating = file_as_fart_rating(FilePath) }.
 
 bump_counter(State, User) ->
-    State.
+    persisted_state(
+      State#state {
+        fart_rating = maps:put(User,
+                               maps:get(User, State#state.fart_rating, 0) + 1,
+                               State#state.fart_rating)
+       }).
 
 as_string(State) ->
-    "".
+    string:join(
+      lists:map(fun ({Name, Counter}) ->
+                        Name ++ ": " ++ integer_to_list(Counter)
+                end,
+        lists:sublist(
+          lists:reverse(
+            lists:keysort(2,
+              maps:to_list(
+                State#state.fart_rating))),
+          1, 10)),
+      ", ").
+
+%% Internal %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+with_dets_file(FilePath, F) ->
+    {ok, Ref} = dets:open_file(FilePath, [{type, set}]),
+    Result = F(Ref),
+    dets:close(Ref),
+    Result.
+
+persisted_state(State) ->
+    option:foreach(
+      fun(FilePath) ->
+              with_dets_file(
+                FilePath,
+                fun(Ref) ->
+                        dets:insert(
+                          Ref,
+                          { fart_rating,
+                            State#state.fart_rating })
+                end)
+      end,
+      State#state.file_path),
+    State.
+
+file_as_fart_rating(FilePath) ->
+    with_dets_file(
+      FilePath,
+      fun(Ref) ->
+              case dets:lookup(Ref, fart_rating) of
+                  [] -> #{};
+                  [{fart_rating, FartRating}] -> FartRating
+              end
+      end).
