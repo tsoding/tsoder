a329a73628c1531194152f5bf18fd2c04a1ab026
rexim <reximkut@gmail.com>
2017-09-07 01:20:51 +0700

    Add zero padding to log filename timestamps (#93)

--- test/logging_tests.erl
+++ test/logging_tests.erl
@@ -7,10 +7,16 @@ utc_timestamp_as_string_test() ->
                    {{12, 34, 56},
                     {78, 90, 12}})).
 
+utc_timestamp_as_string_leading_zeros_test() ->
+    ?assertMatch("12-34-56_78-09-12",
+                 logging:utc_timestamp_as_string(
+                   {{12, 34, 56},
+                    {78, 09, 12}})).
+
 file_name_from_suffix_test() ->
     ?assertMatch("log_hello.txt",
                 logging:file_name_from_suffix("hello")).
 
 file_path_from_timestamp_test() ->
-    ?assertMatch("./logs/log_2017-7-19_12-31-23.txt",
+    ?assertMatch("./logs/log_2017-07-19_12-31-23.txt",
                  logging:file_path_from_timestamp({1500,467483,785624})).
