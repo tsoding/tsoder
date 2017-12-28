58de5fd47924dbf79bad76b6578f72e562830998
rexim <reximkut@gmail.com>
2017-12-29 00:52:40 +0700

    Implement memigrate module for mnesia migrations (#107)

--- src/tsoder_app.erl
+++ src/tsoder_app.erl
@@ -11,6 +11,7 @@
 -export([start/2, stop/1]).
 -include("fart_rating.hrl").
 -include("quote_database.hrl").
+-include("unique_ids.hrl").
 
 %%====================================================================
 %% API
@@ -19,6 +20,7 @@
 
 start(_StartType, _StartArgs) ->
     start_logging(),
+    memigrate:migrate(migrations()),
     ok = mnesia:wait_for_tables([ unique_ids
                                 , fart_rating
                                 , quote
@@ -39,3 +41,17 @@ start_logging() ->
         logging:file_path_from_timestamp(erlang:timestamp()),
     filelib:ensure_dir(LogFilePath),
     error_logger:logfile({open, LogFilePath}).
+
+migrations() ->
+    [{1, fun() ->
+                 mnesia:create_table(unique_ids,
+                                     [{attributes, record_info(fields, unique_ids)},
+                                      {disc_only_copies, [node()]}]),
+                 mnesia:create_table(fart_rating,
+                                     [{attributes, record_info(fields, fart_rating)},
+                                      {disc_only_copies, [node()]}]),
+                 mnesia:create_table(quote,
+                                     [{attributes, record_info(fields, quote)},
+                                      {disc_only_copies, [node()]}]),
+                 ok
+         end}].
