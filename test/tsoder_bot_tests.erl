9efdb1613bf6591301c53dd1e1c39884c3d21d8d
rexim <reximkut@gmail.com>
2018-01-01 15:06:18 +0700

    Decompose the tsoder_bot_tests:join_test_ (#50)

--- test/tsoder_bot_tests.erl
+++ test/tsoder_bot_tests.erl
@@ -1,55 +1,88 @@
 -module(tsoder_bot_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-%% TODO(#50): decompose and rename the join unit test of tsoder_bot
+message_test_() ->
+    tsoder_bot_fixture(
+      [{timeout, 1,
+        ?_test(begin
+                   ?assertMatch(
+                      unsupported,
+                      gen_server:call(tsoder_bot, unknown_event))
+               end)},
+       {timeout, 1,
+        ?_test(begin
+                   ?assertMatch(
+                      nomessage,
+                      gen_server:call(
+                        tsoder_bot,
+                        {message, "khooy", "Just a regular message"}))
+               end)}]).
+
 join_test_() ->
+    tsoder_bot_fixture(
+      [{timeout, 1,
+        ?_test(begin
+                   ?assertMatch(
+                      {message, "I came tsodinNERD"},
+                      gen_server:call(tsoder_bot, {join, self()}))
+               end)}]).
+
+hi_command_test_() ->
+    tsoder_bot_fixture(
+      [{timeout, 1,
+        ?_test(begin
+                   ?assertMatch(
+                      {message, "Hello @khooy!"},
+                      gen_server:call(tsoder_bot, {message, "khooy", "!hi"}))
+               end)}]).
+
+help_command_test_() ->
+    tsoder_bot_fixture(
+      [{timeout, 1,
+        ?_test(begin
+                   ?assertMatch(
+                        { message,
+                          "@khooy, "
+                          "supported commands: "
+                          "addcom, addquote, delcom, "
+                          "fart, help, hi, quote, russify"
+                          ". Source code: https://github.com/tsoding/tsoder" },
+                      flatten_message(
+                        gen_server:call(
+                          tsoder_bot,
+                          {message, "khooy", "!help"})))
+               end)},
+       {timeout, 1,
+        ?_test(begin
+                   ?assertMatch(
+                      { message,
+                        "@khooy, "
+                        "!help [command] -- "
+                        "prints the list of supported commands."},
+                      flatten_message(
+                        gen_server:call(
+                          tsoder_bot,
+                          {message, "khooy", "!help help"})))
+               end)},
+       {timeout, 1,
+        ?_test(begin
+                   ?assertMatch(
+                      { message,
+                        "@khooy, never heard of khooy" },
+                      flatten_message(
+                        gen_server:call(
+                          tsoder_bot,
+                          {message, "khooy", "!help khooy"})))
+               end)}]).
+
+%% INTERNAL %%
+
+tsoder_bot_fixture(Test) ->
     {setup,
-     fun() -> tsoder_bot:start_link()
-     end,
-     fun(_) -> gen_server:stop(tsoder_bot) end,
-     fun(_) ->
-             [{timeout, 1,
-               ?_test(begin
-                          ?assertMatch({message, "I came tsodinNERD"},
-                                       gen_server:call(tsoder_bot, {join, self()}))
-                      end)},
-              {timeout, 1,
-               ?_test(begin
-                          ?assertMatch({message, "Hello @khooy!"},
-                                       gen_server:call(tsoder_bot, {message, "khooy", "!hi"}))
-                      end)},
-              {timeout, 1,
-               ?_test(begin
-                          ?assertMatch({message, [ "@"
-                                                 , "khooy"
-                                                 , ", "
-                                                 , "supported commands: addcom, addquote, delcom, fart, help, hi, quote, russify. Source code: https://github.com/tsoding/tsoder"]},
-                                       gen_server:call(tsoder_bot, {message, "khooy", "!help"}))
-                      end)},
-              {timeout, 1,
-               ?_test(begin
-                          ?assertMatch({message, [ "@"
-                                                 , "khooy"
-                                                 , ", "
-                                                 , "!help [command] -- prints the list of supported commands."]},
-                                       gen_server:call(tsoder_bot, {message, "khooy", "!help help"}))
-                      end)},
-              {timeout, 1,
-               ?_test(begin
-                          ?assertMatch({message, [ "@"
-                                                 , "khooy"
-                                                 , ", "
-                                                 , "never heard of khooy"]},
-                                       gen_server:call(tsoder_bot, {message, "khooy", "!help khooy"}))
-                      end)},
-              {timeout, 1,
-               ?_test(begin
-                          ?assertMatch(unsupported,
-                                       gen_server:call(tsoder_bot, unknown_event))
-                      end)},
-              {timeout, 1,
-               ?_test(begin
-                          ?assertMatch(nomessage,
-                                       gen_server:call(tsoder_bot, {message, "khooy", "Just a regular message"}))
-                      end)}]
-     end}.
+     fun tsoder_bot:start_link/0,
+     fun (_) -> gen_server:stop(tsoder_bot) end,
+     Test}.
+
+flatten_message({message, Message}) ->
+    {message, lists:flatten(Message)};
+flatten_message(X) -> X.
